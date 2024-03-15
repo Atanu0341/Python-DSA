4a9affdab8741b5f6b29f2a277fc55277605db50
Atanu Majumder <atanumajumder2004@gmail.com>
2024-03-15 11:11:13 +0530

    Add zeroMatrix function with better approach

---
+++ Arrays/Zero-Matrix(Coding-Ninja)/Better-Approach.py
@@ -0,0 +1,27 @@
+from sys import *
+from collections import *
+from math import *
+
+def zeroMatrix(matrix, n, m):
+    # better approach
+    row = [0] * n  # row array
+    col = [0] * m  # col array
+
+    # Traverse the matrix
+    for i in range (n):
+        for j in range (m):
+            if matrix[i][j] == 0:
+                # mark ith index of row wih 1:
+                row[i]=1
+
+                # mark jth index of col wih 1:
+                col[j]=1
+
+    # Finally, mark all (i, j) as 0
+    # if row[i] or col[j] is marked with 1.   
+    for i in range(n):
+        for j in range(m):
+            if row[i] or col[j]:
+                matrix[i][j] = 0
+
+    return matrix
