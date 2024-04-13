fd5e365973c9230983c913b3a4ca1e80f25ed3c5
Atanu Majumder <atanumajumder2004@gmail.com>
2024-04-13 10:56:23 +0530

    Refactor lowerBound function implementation in Solution.cpp

--- BinarySearch/Implement-Lower-Bound/Solution.cpp
+++ BinarySearch/Implement-Lower-Bound/Solution.cpp
@@ -27,3 +27,27 @@ int lowerBound(vector<int> arr, int n, int x) {
 	}
 	return start;
 }
+
+
+// or
+
+int lowerBound(vector<int> arr, int n, int x) {
+	int low = 0, high = n - 1;
+    int ans = n;
+    
+
+    while (low <= high) {
+        int mid = low + (high - low)/2;
+        // maybe an answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+
+}
