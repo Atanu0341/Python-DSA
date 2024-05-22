ec9819f146426b2eb1274bfe3a7fb4fd0999960f
Atanu Majumder <atanumajumder2004@gmail.com>
2024-05-22 11:37:27 +0530

    Refactor linearSearch.cpp for better readability and maintainability

--- Recurssion/linearSearch.cpp
+++ Recurssion/linearSearch.cpp
@@ -1,36 +1,53 @@
 #include <iostream>
 using namespace std;
 
-bool search(int arr[], int size, int key){
+void print(int arr[], int n)
+{
+    cout << "Size of array is " << n << endl;
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] <<" ";
+    }
+    cout << endl;
+}
+
+bool search(int arr[], int size, int key)
+{
+    print(arr, size);
     // base case
-    if(size == 0){
+    if (size == 0)
+    {
         return false;
     }
 
-    if(arr[0] == key){
+    if (arr[0] == key)
+    {
         return true;
     }
-    else{
-        bool remainingPart = search(arr+1, size-1, key);
+    else
+    {
+        bool remainingPart = search(arr + 1, size - 1, key);
         return remainingPart;
     }
 }
 
-int main() {
+int main()
+{
 
-    int arr[5] = {3,5,1,2,6};
+    int arr[5] = {3, 5, 1, 2, 6};
     int size = 5;
-    int key = 2;
+    int key = 6;
     bool ans = search(arr, size, key);
 
-    if(ans){
-        cout<<"Present"<<endl;
+    if (ans)
+    {
+        cout << "Present" << endl;
     }
-    else{
-        cout<<"Not Present" <<endl;
+    else
+    {
+        cout << "Not Present" << endl;
     }
-
 
     return 0;
 }
