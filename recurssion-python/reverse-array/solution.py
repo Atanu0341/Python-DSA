671a1c50c020e2047a5a88123d7d8e07b48f8dce
Atanu Majumder <atanumajumder2004@gmail.com>
2024-06-21 07:44:21 +0000

    Refactor reverse array function for better readability and maintainability

--- recurssion-python/reverse-array/solution.py
+++ recurssion-python/reverse-array/solution.py
@@ -1,19 +1,40 @@
-def f(a, l, r):
-    if l >= r:
+# two pointer
+
+# def f(a, l, r):
+#     if l >= r:
+#         return
+
+#     # Swap elements at indices l and r
+#     a[l], a[r] = a[r], a[l]
+
+#     # Recursive call
+#     f(a, l+1, r-1)
+
+# one pointer
+
+def f(a,i):
+
+    if i >=n/2:
         return
+    
+    # swap
+    a[i], a[n-i-1] = a[n-i-1],a[i]
 
-    # Swap elements at indices l and r
-    a[l], a[r] = a[r], a[l]
-
-    # Recursive call
-    f(a, l+1, r-1)
+    # recursive call
+    f(a, i+1)
 
 # Initialize the array
 arr = [1, 3, 2, 5, 4]
 n = len(arr)
 
 # Call the function to reverse the array
-f(arr, 0, n-1)
+# f(arr, 0, n-1)  in case of two pointer
+f(arr, 0)
 
 # Print the reversed array
 print(arr)
+
+
+
+
+
