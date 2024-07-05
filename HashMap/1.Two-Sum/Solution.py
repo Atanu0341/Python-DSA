e2c25982fdaf4a7a9d167a6f0ff54df05dd02976
Atanu Majumder <atanumajumder2004@gmail.com>
2024-07-05 06:27:55 +0000

    Update variable names for clarity in Solution.py

--- HashMap/1.Two-Sum/Solution.py
+++ HashMap/1.Two-Sum/Solution.py
@@ -4,17 +4,17 @@ class Solution:
         prevMap = {}  # values:index will store here
 
         # Iterate through the list 'nums' along with their indices
-        for i, n in enumerate(nums):
+        for index, value in enumerate(nums):
             # Calculate the difference needed to reach the target from the current number
-            diff = target - n
+            diff = target - value
 
             # Check if the required difference exists in the dictionary
             if diff in prevMap:
                 # If found, return the indices of the two numbers that add up to the target
-                return [prevMap[diff], i]
+                return [prevMap[diff], index]
 
             # If the difference is not found in the dictionary, store the current number    and its index
-            prevMap[n] = i
+            prevMap[value] = index
 
         # If no such pair is found after iterating through the entire list, return None
         return None
